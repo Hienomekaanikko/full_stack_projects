8654277973e7ee353d64ee5af7a62d6439e2663f
mikkosuokas <mikesuokas@gmail.com>
2025-11-06 13:39:02 +0200

    base for tailwind practice. Use with new branches

---
+++ tailwind_base/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white text-4xl font-bold">
+      Tailwind v4 is working 🎉
+    </div>
+  );
+}
